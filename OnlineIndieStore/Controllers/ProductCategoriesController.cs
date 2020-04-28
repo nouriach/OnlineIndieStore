ecd9d641549aa4aa87c18df8652b56e7b4c5153b
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-04-28 18:06:05 +0100

    Revert "revert changes"
    This reverts commit 2908ab1a5a1f523105c37a07503927cd33e9c3f3.

--- OnlineIndieStore/Controllers/ProductCategoriesController.cs
+++ OnlineIndieStore/Controllers/ProductCategoriesController.cs
@@ -20,37 +20,25 @@ namespace OnlineIndieStore.Controllers
         }
 
         // GET: ProductCategories
-        public async Task<IActionResult> Index(string? order, string? selectionOrder, string? categoryOrder)
+        public async Task<IActionResult> Index(string? order)
         {
             var appDbContext = _context.ProductCategories
                 .Include(p => p.Category)
                 .Include(p => p.Product);
-                //.Include(s => s.Selection);
-
 
             List<string> categories = Enum.GetNames(typeof(CategoryName)).OrderBy(x => x).ToList();
             ViewBag.CatOptions = categories;
+
             List<string> selections = Enum.GetNames(typeof(Selection)).OrderBy(y => y).ToList();
             ViewBag.SelOptions = selections;
-
-            // Don't know where to continue
-
-            //if (!String.IsNullOrEmpty(selectionOrder))
-            //{
-            //    return View(
-            //        await appDbContext
-            //        .OrderBy(x => x)
-            //        .ToListAsync()
-            //        );
-            //}
 
             switch (order)
             {
             case "ByPriceAscending":
                 return View(
                     await appDbContext
-                        .OrderBy(x => x.Product.Price)
-                        .ToListAsync()
+                    .OrderBy(x => x.Product.Price)
+                    .ToListAsync()
                     );
             case "ByPriceDescending":
                 return View(
@@ -65,11 +53,7 @@ namespace OnlineIndieStore.Controllers
                     .ToListAsync()
                     );
             default:
-                return View(
-                    await appDbContext
-                    .OrderBy(x => x.Product.Name)
-                    .ToListAsync()
-                    );
+                return View(await appDbContext.OrderBy(x => x.Product.Name).ToListAsync());
             }
         }
 
@@ -106,6 +90,8 @@ namespace OnlineIndieStore.Controllers
         }
 
         // POST: ProductCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ProductCategoryID,ProductID,CategoryID,Product, Category, Selection")] ProductCategory productCategory)
