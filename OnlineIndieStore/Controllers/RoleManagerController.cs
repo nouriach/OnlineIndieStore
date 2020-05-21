f198f076c2e2f1f786a08fe233bee131cb83fe14
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-05-21 09:53:07 +0100

    Setup Create Role admin area

---
+++ OnlineIndieStore/Controllers/RoleManagerController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineIndieStore.Models;
+using OnlineIndieStore.VMs;
+
+namespace OnlineIndieStore.Controllers
+{
+    public class RoleManagerController : Controller
+    {
+        public RoleManager<IdentityRole> _rolesManager { get; set; }
+        private readonly Microsoft.AspNetCore.Identity.UserManager<AppUser> _user;
+
+        public RoleManagerController(RoleManager<IdentityRole> rolesManager, UserManager<AppUser> userManager)
+        {
+            _user = userManager;
+            _rolesManager = rolesManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /***** CREATE NEW ROLES *****/
+
+        [HttpGet]
+        public IActionResult CreateRoles()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRoles(RoleViewModel roleView)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole role = new IdentityRole()
+                {
+                    Name = roleView.RoleName
+                };
+                IdentityResult result = await _rolesManager.CreateAsync(role);
+                    
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListOfRoles", "Rolemanag");
+                }
+                foreach (var identityErrorLE in result.Errors)
+                {
+                    ModelState.AddModelError("", identityErrorLE.Description);
+                }
+            }
+            return View(roleView);
+        }
+    }
+}
+ 
