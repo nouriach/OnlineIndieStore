d5878fdfa40edc2cc1df29f3c06b36d299af92aa
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-05-26 12:27:02 +0100

    Fix 'No matching role' duplication on View

--- OnlineIndieStore/Controllers/RoleManagerController.cs
+++ OnlineIndieStore/Controllers/RoleManagerController.cs
@@ -199,8 +199,9 @@ namespace OnlineIndieStore.Controllers
             List<EditsUsersInRolesViewModel> userRoleList = new List<EditsUsersInRolesViewModel>();
 
             var matchingUser = await _user.FindByIdAsync(id);
-            ViewBag.User = matchingUser.FirstName.ToString();
-
+            ViewBag.FirstName = matchingUser.FirstName;
+            ViewBag.UserName = matchingUser.UserName ;
+            ViewBag.Email = matchingUser.Email ;
 
             if (matchingUser == null)
             {
@@ -224,28 +225,6 @@ namespace OnlineIndieStore.Controllers
                 }
                 userRoleList.Add(userRole);
             }
-
-            // In the blog it suggests the View needs to have a List<ViewModel>
-            // 1. The model in here that is instantiated needs to be a List<EditsUsersInRolesViewModel>
-            // 2. Then for every role in the system, do a for loop which
-            /***
-            a.Creates a new instance of EditsUsersInRolesViewModel()
-            b.Adds the matching role to the viewmodel
-            c.Then checks whether the role is already assigned to the User defined at the top (matchingUser)
-            d.If it is assigned, then mark the 'IsSelected' bool as 'true' in the viewmodel and vice versa
-            e.then after each instance of the model has been added with a true or a false, add it to the List<EditsUsersInRolesViewModel>
-            ***/
-
-            // link: https://sagarjaybhay.com/rolemanager-in-asp-net-core-2019/
-
-            // So what does the view model look like?
-            /*****
-                public string Id { get; set; }
-                public AppUser AppUser { get; set; } (maybe not...? It looks like the UserID should be stored as an ID in the ViewBag)
-                public IdentityRole IdentityRole { get; set; }
-                public bool IsChecked { get; set; }
-             *****/
-
             return View(userRoleList);
         }
 
@@ -288,66 +267,3 @@ namespace OnlineIndieStore.Controllers
         }
     }
 }
-
-
-/************
-
-    HTTP GET:
-
-    List<IdentityRole> userIdentityRoles = new List<IdentityRole>();
-    List<IdentityRole> availableIdentityRoles = new List<IdentityRole>();
-
-    foreach (var role in this.rolesManager.Roles)
-    {
-        availableIdentityRoles.Add(role);
-
-        if (await _user.IsInRoleAsync(matchingUser, role.ToString()))
-        {
-            userIdentityRoles.Add(role);
-        }
-    }
-    EditsUsersInRolesViewModel model = new EditsUsersInRolesViewModel()
-    {
-        Id = (matchingUser.Id),
-        AppUser = matchingUser,
-        IdentityRoles = userIdentityRoles
-    };
-
-    ViewBag.AvailableRoles = availableIdentityRoles;
-
-    HTTP POST
-
-    else
-            {
-                // List all roles
-                List<IdentityRole> availableIdentityRoles = new List<IdentityRole>();
-                // initiate a new list of IdentityRoles
-                List<IdentityRole> ir = new List<IdentityRole>();
-                // Create new Role Model to pass to View, add the relevant user
-                EditsUsersInRolesViewModel userRoleModel = new EditsUsersInRolesViewModel()
-                {
-                    Id = (user.Id),
-                    AppUser = user,
-                    IsChecked = false
-                };
-                
-
-                // scan through the List<string> admin roles elected and store in a new List
-                var checkedRoles = IsChecked.Where(x => x != "false").Select(x => x);
-
-                // loop through the stored Roles in the database
-                foreach (var r in this.rolesManager.Roles)
-                {
-                    availableIdentityRoles.Add(r);
-                    
-                    // if the stored roles in the database are also included in the checked roles add to the new View Model instance
-                    if (checkedRoles.Contains(r.ToString()))
-                    {
-                        
-                        ir.Add(r);
-                    }
-                }
-                userRoleModel.IdentityRoles = ir;
-
-            ViewBag.AvailableRoles = availableIdentityRoles;
- */
