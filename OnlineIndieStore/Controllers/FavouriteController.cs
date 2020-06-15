129afcde9581150fb8889c850d61537f6ba0ef61
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-06-15 09:39:51 +0100

    Set up 'Favourites' Controller & View

---
+++ OnlineIndieStore/Controllers/FavouriteController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineIndieStore.Controllers
+{
+    public class FavouriteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
